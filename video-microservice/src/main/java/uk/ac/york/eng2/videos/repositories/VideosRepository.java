0b23919214927613725e6787c8a56210be50634f
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2023-12-08 14:54:32 +0000

    removed db joins in vidrepo

--- video-microservice/src/main/java/uk/ac/york/eng2/videos/repositories/VideosRepository.java
+++ video-microservice/src/main/java/uk/ac/york/eng2/videos/repositories/VideosRepository.java
@@ -15,8 +15,8 @@ import uk.ac.york.eng2.videos.dto.VideoDTO;
 @Repository
 public interface VideosRepository extends CrudRepository<Video, Long> {
 
-	@Join(value = "watchers", type = Join.Type.LEFT_FETCH)
-	@Join(value = "author", type = Join.Type.LEFT_FETCH)
+//	@Join(value = "watchers", type = Join.Type.LEFT_FETCH)
+//	@Join(value = "author", type = Join.Type.LEFT_FETCH)
 	@Override
 	Optional<Video> findById(@NotNull Long id);
 
