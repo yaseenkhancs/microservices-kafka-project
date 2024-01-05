374ca3241db1957d4c7167266a875da906156407
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2024-01-05 19:25:13 +0000

    added reading hashtag and user as consumers

--- subscription-microservice/src/main/java/uk/ac/york/eng2/subscription/events/PostedVideosConsumer.java
+++ subscription-microservice/src/main/java/uk/ac/york/eng2/subscription/events/PostedVideosConsumer.java
@@ -3,6 +3,8 @@ package uk.ac.york.eng2.subscription.events;
 import io.micronaut.configuration.kafka.annotation.KafkaKey;
 import io.micronaut.configuration.kafka.annotation.KafkaListener;
 import io.micronaut.configuration.kafka.annotation.Topic;
+import uk.ac.york.eng2.subscription.domain.Hashtag;
+import uk.ac.york.eng2.subscription.domain.User;
 import uk.ac.york.eng2.subscription.domain.Video;
 
 @KafkaListener(groupId="books-debug")
@@ -12,5 +14,14 @@ public class PostedVideosConsumer {
 	public void videoPostedMetric(@KafkaKey long k, Video v) {
 		System.out.println(v.getTitle());
 	}
-
+	
+	@Topic("user-added")
+	public void addedUser(@KafkaKey long k, User u) {
+		System.out.println(u.getUsername());
+	}
+	
+	@Topic("hashtag-added")
+	public void addedHashtag(@KafkaKey long k, Hashtag h) {
+		System.out.println(h.getName());
+	}
 }
