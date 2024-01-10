e35cd613b65c477233d48b3ef196b9c2b0836133
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2024-01-10 21:56:45 +0000

    added video CLI test

--- video-cli/src/test/java/uk/ac/york/eng2/videos/cli/VideoCliCommandTest.java
+++ video-cli/src/test/java/uk/ac/york/eng2/videos/cli/VideoCliCommandTest.java
@@ -25,4 +25,30 @@ public class VideoCliCommandTest {
             assertTrue(baos.toString().contains("Hi!"));
         }
     }
+    
+    @Test
+    public void postThenGetUser() throws Exception {
+    	try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
+    		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    		System.setOut(new PrintStream(baos));
+    		PicocliRunner.run(VideoCliCommand.class, ctx, new String[] {"add-user", "WHAT"});
+    		assertTrue(baos.toString().contains("CREATED"), "The new book was created");
+//            PicocliRunner.run(VideoCliCommand.class, ctx, args);
+
+            // video-cli
+//            assertTrue(baos.toString().contains("Hi!"));
+    		
+    		baos = new ByteArrayOutputStream();
+    		System.setOut(new PrintStream(baos));
+    		PicocliRunner.run(VideoCliCommand.class, ctx, new String[] {"get-users"});
+    		final String newOutput = baos.toString().trim();
+    		final String[] newLines = newOutput.split(System.lineSeparator());
+    		int lineNumber = 0;
+    		assertTrue(newLines.length > 0);
+    		for (String line : newLines) {
+    			++lineNumber;
+    			assertTrue(line.startsWith("User"));
+    		}
+        }
+    }
 }
