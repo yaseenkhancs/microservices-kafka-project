c3e36e73315dd3ab42688db3f90b656d868fbf54
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2024-01-11 18:55:56 +0000

    changed ymls and deleted sql

--- video-microservice/sql/extra-database.sql
+++
@@ -1,7 +0,0 @@
-create database db2
-
-grant all privileges on extra_database.* to 'mydo'@'%' identified by 'mydosecret'
-
-create database db3
-
-grant all privileges on extra_database.* to 'threedo'@'%' identified by 'threedosecret'
