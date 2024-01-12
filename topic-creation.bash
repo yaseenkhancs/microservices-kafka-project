8d20c11dfc810ace58e98179415afb08bf92724e
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2024-01-12 17:12:44 +0000

    this works

--- topic-creation.bash
+++ topic-creation.bash
@@ -8,3 +8,7 @@ docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-0 
 docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-0 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic video-liked-by-hour --replication-factor 3 --partitions 6
 docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-1 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic user-subscribed --replication-factor 3 --partitions 6
 docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-2 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic user-unsubscribed --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-2 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic videos-metrics-liked-by-hour --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-2 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic videos-metrics-liked-by-hour-repartition --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-2 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic videos-metrics-aggregated-repartition --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-2 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic subs-stream-liked-by-hour-repartition --replication-factor 3 --partitions 6
