dbadf88325052d009154b9d9095dcd4992035ef4
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2024-01-18 21:00:23 +0000

    completely removed kstreams

--- trending-hashtag-microservice/src/main/java/uk/ac/york/eng2/trending/helpers/Hashtag.java
+++ trending-hashtag-microservice/src/main/java/uk/ac/york/eng2/trending/helpers/Hashtag.java
@@ -1,5 +1,23 @@
 package uk.ac.york.eng2.trending.helpers;
 
-public class Hashtag {
 
+public class Hashtag {
+	private Long id;
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
