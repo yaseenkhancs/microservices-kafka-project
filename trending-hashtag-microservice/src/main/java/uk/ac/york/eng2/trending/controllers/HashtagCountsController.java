03725828406f7bc5a03193915c3626099701e5ed
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2024-01-18 21:40:39 +0000

    implemented saving to new hashtagcounts db

--- trending-hashtag-microservice/src/main/java/uk/ac/york/eng2/trending/controllers/HashtagCountsController.java
+++ trending-hashtag-microservice/src/main/java/uk/ac/york/eng2/trending/controllers/HashtagCountsController.java
@@ -1,9 +1,14 @@
 package uk.ac.york.eng2.trending.controllers;
 
+import java.net.URI;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
 import jakarta.inject.Inject;
-import uk.ac.york.eng2.trending.domain.Hashtag;
+import uk.ac.york.eng2.trending.helpers.Hashtag;
 import uk.ac.york.eng2.trending.domain.HashtagCounts;
 import uk.ac.york.eng2.trending.repositories.HashtagCountsRepository;
 
@@ -16,5 +21,14 @@ public class HashtagCountsController {
 	@Get("/")
 	public Iterable<HashtagCounts> list() {
 		return repo.findAll();
+	}
+	
+	@Post("/")
+	public HttpResponse<Void> add(@Body Hashtag tagdetails) {
+		HashtagCounts tag = new HashtagCounts();
+		tag.setName(tagdetails.getName());
+		tag.setTimeStamp(System.currentTimeMillis());
+		repo.save(tag);
+		return HttpResponse.created(URI.create("/hashtagcounts/" + tag.getId()));
 	}
 }
--- trending-hashtag-microservice/src/main/java/uk/ac/york/eng2/trending/events/LikedVideosConsumer.java
+++ trending-hashtag-microservice/src/main/java/uk/ac/york/eng2/trending/events/LikedVideosConsumer.java
@@ -7,22 +7,18 @@ import io.micronaut.configuration.kafka.annotation.KafkaKey;
 import io.micronaut.configuration.kafka.annotation.KafkaListener;
 import io.micronaut.configuration.kafka.annotation.Topic;
 import jakarta.inject.Inject;
-import uk.ac.york.eng2.trending.controllers.HashtagsController;
+import uk.ac.york.eng2.trending.controllers.HashtagCountsController;
 import uk.ac.york.eng2.trending.helpers.Hashtag;
 
 @KafkaListener(groupId = "books-debug")
 public class LikedVideosConsumer {	
 	
 	@Inject
-	HashtagsController control;
+	HashtagCountsController control;
 			
 	@Topic("video-liked")
 	public void videoLikeMetric(@KafkaKey long v, Hashtag h) {
 		System.out.println("Haha");
+		control.add(h);
 	}
-	
-//	@Topic("video-liked-by-hour")
-//	public void videoLikeHourMetric(@KafkaKey long v, Hashtag h) {
-//		System.out.println("Haha");
-//	}
 }
