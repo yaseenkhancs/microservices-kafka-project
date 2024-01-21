1ff0f5915d1340d385f330c270b2bd2392cf77a0
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2024-01-21 02:33:00 +0000

    topic-creation script generated, egx tweaked to correct filepath

---
+++ modeling/m2t/uk.ac.york.eng2.assessment.y3884331.m2t/other-generated-files/topic-creation.bash
@@ -0,0 +1,10 @@
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-0 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic video-watched --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-0 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic video-liked --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-0 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic video-disliked --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-0 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic video-posted --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-0 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic user-added --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-0 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic hashtag-added --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-0 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic another-hashtag --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-0 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic watcher-added --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-0 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic user-subscribed --replication-factor 3 --partitions 6
+docker compose -p assessment-prod -f compose-prod.yml exec -e JMX_PORT= kafka-0 kafka-topics.sh --bootstrap-server kafka-0:9092 --create --topic user-unsubscribed --replication-factor 3 --partitions 6
