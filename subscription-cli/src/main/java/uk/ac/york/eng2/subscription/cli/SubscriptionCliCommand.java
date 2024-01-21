6574ede2866372aa2e2361a832537a5b7848aa08
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2024-01-21 02:08:04 +0000

    deleted unnecessary subscription commands

--- subscription-cli/src/main/java/uk/ac/york/eng2/subscription/cli/SubscriptionCliCommand.java
+++ subscription-cli/src/main/java/uk/ac/york/eng2/subscription/cli/SubscriptionCliCommand.java
@@ -4,33 +4,19 @@ import io.micronaut.configuration.picocli.PicocliRunner;
 
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-import uk.ac.york.eng2.subscription.cli.hashtags.AddHashtagCommand;
-import uk.ac.york.eng2.subscription.cli.hashtags.GetHashtags;
 import uk.ac.york.eng2.subscription.cli.hashtags.ViewHashtagVideos;
 import uk.ac.york.eng2.subscription.cli.health.HeartBeatCommand;
 import uk.ac.york.eng2.subscription.cli.users.AddSubscriptionCommand;
-import uk.ac.york.eng2.subscription.cli.users.AddUserCommand;
 import uk.ac.york.eng2.subscription.cli.users.DeleteSubscriptionCommand;
-import uk.ac.york.eng2.subscription.cli.users.GetUsersCommand;
 import uk.ac.york.eng2.subscription.cli.users.ViewSubscriptions;
 import uk.ac.york.eng2.subscription.cli.users.ViewUserSubscriptionVideos;
 import uk.ac.york.eng2.subscription.cli.users.ViewWatchedVideos;
-import uk.ac.york.eng2.subscription.cli.videos.AddVideoCommand;
-import uk.ac.york.eng2.subscription.cli.videos.AddVideoHashtag;
-import uk.ac.york.eng2.subscription.cli.videos.AddVideoWatcherCommand;
-import uk.ac.york.eng2.subscription.cli.videos.DislikeVideoCommand;
-import uk.ac.york.eng2.subscription.cli.videos.GetVideosCommand;
-import uk.ac.york.eng2.subscription.cli.videos.LikeVideoCommand;
 
 @Command(name = "subscription-cli", description = "...",
-        mixinStandardHelpOptions = true, subcommands = {GetVideosCommand.class,
-        		GetHashtags.class, GetUsersCommand.class, AddVideoCommand.class,
-        		AddUserCommand.class, AddHashtagCommand.class, AddVideoHashtag.class,
-        		AddVideoWatcherCommand.class, LikeVideoCommand.class, 
-        		DislikeVideoCommand.class, AddSubscriptionCommand.class,
-        		ViewHashtagVideos.class, ViewSubscriptions.class, ViewWatchedVideos.class,
-        		ViewUserSubscriptionVideos.class, DeleteSubscriptionCommand.class,
-        		HeartBeatCommand.class})
+        mixinStandardHelpOptions = true, subcommands = {        		
+        		AddSubscriptionCommand.class, ViewHashtagVideos.class, ViewSubscriptions.class, 
+        		ViewWatchedVideos.class, ViewUserSubscriptionVideos.class, 
+        		DeleteSubscriptionCommand.class, HeartBeatCommand.class})
 public class SubscriptionCliCommand implements Runnable {
 
     @Option(names = {"-v", "--verbose"}, description = "...")
---
+++ video-cli/subscriptiontest.bash
@@ -0,0 +1,4 @@
+./gradlew run --args="add-user 'yaseen'"
+./gradlew run --args="add-tag 'sad'"
+./gradlew run --args="post-video 'lolvideo' 1 2"
+./gradlew run --args="add-user 'Greg'"
